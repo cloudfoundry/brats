46cb819085b642a42a37c536cd30ca12f64a83ca
Sai To Yeung <pair+syeung@pivotal.io>
2015-03-12 13:56:52 -0400

    Use flags in tests script to reflect buildpack-build

---
+++ cf_spec/fixtures/python/simple/server.py
@@ -0,0 +1,23 @@
+import sys
+
+if int(sys.version[0]) >= 3:
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+else:
+    from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+
+port = int(sys.argv[1])
+
+class requestHandler(BaseHTTPRequestHandler):
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html>")
+        s.wfile.write("<head><title>Python app.</title></head>")
+        s.wfile.write("<body><p>Hello, World</p></body>")
+        s.wfile.write("</html>")
+        return
+
+web_server = HTTPServer(('0.0.0.0', port), requestHandler)
+print('Started on port', port)
+web_server.serve_forever()
---
+++ cf_spec/integration/python_spec.rb
@@ -0,0 +1,68 @@
+require_relative '../spec_helper'
+require 'yaml'
+
+describe 'For all supported Python versions' do
+  def self.create_test_for(test_name, options={})
+    options[:engine_version] ||= options[:version]
+
+    context "On #{ENV['CF_STACK']} with #{test_name}" do
+      let(:app_name) { 'python' }
+      let(:version) { options[:version] }
+      let(:app) { Machete.deploy_app("python/tmp/#{version}/simple") }
+      let(:browser) { Machete::Browser.new(app) }
+
+      specify do
+        generate_app('simple', version)
+        assert_correct_version_installed(version)
+
+        assert_root_contains('Hello, World')
+        assert_offline_mode_has_no_traffic
+      end
+    end
+  end
+
+  dependencies = YAML::load_file('manifests/python/manifest.yml')['dependencies']
+
+  dependencies.each do |dependency|
+    if dependency['cf_stacks'].include?(ENV['CF_STACK']) && dependency['name'] == 'python'
+      create_test_for("#{dependency['name']} #{dependency['version']}", version: dependency['version'])
+    end
+  end
+
+
+  def assert_offline_mode_has_no_traffic
+    expect(app.host).not_to have_internet_traffic if Machete::BuildpackMode.offline?
+  end
+
+  def generate_app(app_name, version)
+    origin_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'python', app_name)
+		copied_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'python', 'tmp', version.to_s, app_name)
+    FileUtils.rm_rf(copied_template_path)
+    FileUtils.mkdir_p(File.dirname(copied_template_path))
+    FileUtils.cp_r(origin_template_path, copied_template_path)
+
+    ['runtime.txt'].each do |file|
+      evaluate_erb(File.join(copied_template_path, file), binding)
+    end
+  end
+
+  def assert_correct_version_installed(version)
+    expect(app).to be_running
+    expect(app).to have_logged "Installing runtime (python-#{version})"
+  end
+
+
+  def assert_root_contains(text)
+    2.times do
+      browser.visit_path('/')
+      expect(browser).to have_body(text)
+    end
+  end
+
+  def evaluate_erb(file_path, our_binding)
+    template = File.read(file_path)
+    f = File.open(file_path, 'w')
+    f << ERB.new(template).result(our_binding)
+    f.close
+  end
+end
