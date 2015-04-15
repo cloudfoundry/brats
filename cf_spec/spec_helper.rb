4361bc45845bcd7c76aa893ce4633dc374ad3f6b
JT Archie <jtarchie@gmail.com>
2015-04-15 16:00:31 -0400

    remove code that is not used

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -4,7 +4,5 @@ require 'machete/matchers'
 require 'open-uri'
 require 'json'
 
-Dir[File.join(File.dirname(__FILE__), 'support', '**', '*.rb')].each { |f| require f }
-
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
