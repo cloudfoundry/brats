151a4a70667a9374facb8c3baa8b9222d294f638
JT Archie <jtarchie@gmail.com>
2015-06-09 17:30:00 -0400

    use BRATS_BRANCH when checking out buildpack
    Signed-off-by: Matt Horan <mhoran@pivotal.io>

--- cf_spec/spec_helper.rb
+++ cf_spec/spec_helper.rb
@@ -9,12 +9,14 @@ require 'yaml'
 `mkdir -p log`
 Machete.logger = Machete::Logger.new("log/integration.log")
 
-def parsed_manifest(buildpack:, branch: 'master')
+BRATS_BRANCH = ENV['BRATS_BRANCH'] || 'master'
+
+def parsed_manifest(buildpack:, branch: BRATS_BRANCH)
   manifest_url = "https://raw.githubusercontent.com/cloudfoundry/#{buildpack}-buildpack/#{branch}/manifest.yml"
   YAML.load(open(manifest_url))
 end
 
-def install_buildpack(buildpack:, branch: 'master')
+def install_buildpack(buildpack:, branch: BRATS_BRANCH)
   FileUtils.mkdir_p('tmp')
   ` set -e
     git clone -q -b #{branch} --depth 1 --recursive https://github.com/cloudfoundry/#{buildpack}-buildpack tmp/#{buildpack}-buildpack
