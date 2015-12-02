abcd34d9eceaec40dbb68d761064980a3e55d4e7
Amin Jams <aminjam.software@gmail.com>
2015-12-02 16:37:38 -0500

    Adding coverage for version range for node
    Adding coverage for version range in package.json for node apps. We
    should now be able to test squiggle operator for all versions in
    nodejs-buildpack/manifest.yml.
    [#108988838]
    
    Signed-off-by: David Jahn <david.a.jahn@gmail.com>

--- cf_spec/integration/nodejs_spec.rb
+++ cf_spec/integration/nodejs_spec.rb
@@ -3,34 +3,63 @@ require 'spec_helper'
 module NodeJs
   FIXTURE_DIR  = "#{File.dirname(__FILE__)}/../fixtures/nodejs/simple_brats"
   PACKAGE_JSON = "#{FIXTURE_DIR}/package.json"
+  def self.create_package_json(node_engine)
+    package = {
+      'name' => 'node_web_app',
+      'version' => '0.0.0',
+      'description' => 'hello, world',
+      'main' => 'server.js',
+      'engines' => {
+        'node' => node_engine
+      },
+      'dependencies' => {
+        'bcrypt' => '0.8.5',
+        'bson-ext' => '0.1.13'
+      }
+    }
+    File.open(NodeJs::PACKAGE_JSON, 'w') do |file|
+      file << JSON.generate(package)
+    end
+  end
+end
+
+RSpec.shared_examples :a_deploy_of_nodejs_app_with_version_range do |node_version, stack|
+  context "with node-#{node_version}", version: node_version do
+
+    before :all do
+      NodeJs::create_package_json(node_version)
+      @app = Machete.deploy_app(
+        'nodejs/simple_brats',
+        name: "simple-nodejs-#{Time.now.to_i}",
+        buildpack: 'nodejs-brat-buildpack',
+        stack: stack
+      )
+      @browser = Machete::Browser.new(@app)
+    end
+
+    it 'should be running' do
+      expect(@app).to be_running
+      2.times do
+        @browser.visit_path('/')
+        expect(@browser).to have_body('Hello World!')
+      end
+    end
+
+    it 'should have the correct version' do
+      expect(@app).to_not have_logged /Downloading and installing undefined.../
+      expect(@app).to have_logged "engines.node (package.json):  #{node_version}"
+      expect(@app).to have_logged /Downloading and installing node \d+\.\d+\.\d+/
+    end
+
+  end
 end
 
 RSpec.shared_examples :a_deploy_of_nodejs_app_to_cf do |node_version, stack|
 
   context "with node-#{node_version}", version: node_version do
 
-    def create_package_json(node_engine)
-      package = {
-        'name' => 'node_web_app',
-        'version' => '0.0.0',
-        'description' => 'hello, world',
-        'main' => 'server.js',
-        'engines' => {
-          'node' => node_engine
-        },
-        'dependencies' => {
-          'bcrypt' => '0.8.5',
-          'bson-ext' => '0.1.13'
-        }
-      }
-
-      File.open(NodeJs::PACKAGE_JSON, 'w') do |file|
-        file << JSON.generate(package)
-      end
-    end
-
     before :all do
-      create_package_json(node_version)
+      NodeJs::create_package_json(node_version)
       @app = Machete.deploy_app(
         'nodejs/simple_brats',
         name: "simple-nodejs-#{Time.now.to_i}",
@@ -89,12 +118,19 @@ describe 'Deploying CF apps',:language=> 'nodejs' do
   ['cflinuxfs2'].each do |stack|
     context "on the #{stack} stack", stack: stack do
 
-      nodes.select { |node|
+      all_versions = nodes.select { |node|
         node['cf_stacks'].include?(stack) &&
           Gem::Version.new(node['version']) >= Gem::Version.new('0.10')
-      }.each do |node|
-
+      }
+      all_versions.each do |node|
         it_behaves_like :a_deploy_of_nodejs_app_to_cf, node['version'], stack
+      end
+
+      all_versions.map { |node|
+        version = node['version']
+        "~>" + /(\d+)\.(\d+)/.match(version)[0] + ".0"
+      }.uniq.each do |squiggle_version|
+        it_behaves_like :a_deploy_of_nodejs_app_with_version_range, squiggle_version, stack
       end
     end
   end
