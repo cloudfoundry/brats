acfbb6017a33142a39fbd3356cecdff63c930907
James Wen <jrw2175@columbia.edu>
2016-07-27 15:22:03 -0400

    Refactor and normalize go buildpack specs
    [#126514693]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- cf_spec/integration/go_spec.rb
+++ cf_spec/integration/go_spec.rb
@@ -1,61 +1,55 @@
 require 'spec_helper'
 
+def deploy_app(go_version, stack)
+  template = GoTemplateApp.new(go_version)
+  template.generate!
+
+  Machete.deploy_app(
+    template.path,
+    name: template.name,
+    buildpack: 'go-brat-buildpack',
+    stack: stack
+  )
+end
+
+RSpec.shared_examples :a_deploy_of_go_app_to_cf do |go_version, stack|
+  context "with Go version #{go_version}" do
+    let(:browser) { Machete::Browser.new(@app) }
+
+    before(:all) do
+      @app = deploy_app(go_version, stack)
+    end
+
+    after(:all) { Machete::CF::DeleteApp.new.execute(@app) }
+
+    it 'runs a simple webserver with correct go version' do
+      expect(@app).to be_running(120)
+      expect(@app).to have_logged "Installing go#{go_version}"
+    end
+
+    it 'has content at the root' do
+      2.times do
+        browser.visit_path('/')
+        expect(browser).to have_body('Hello, World')
+      end
+    end
+  end
+end
+
 describe 'For all supported Go versions', language: 'go' do
+  let(:stack) { 'cflinuxfs2' }
+
   before(:all) do
     cleanup_buildpack(buildpack: 'go')
     install_buildpack(buildpack: 'go')
   end
 
-  def self.dependencies
-    parsed_manifest(buildpack: 'go').fetch('dependencies')
-  end
-
-  def self.create_test_for(test_name, options = {})
-    context "with #{test_name}" do
-      let(:version) { options[:version] }
-      let(:app) do
-        template = GoTemplateApp.new(version)
-        template.generate!
-
-        Machete.deploy_app(
-          template.path,
-          name: template.name,
-          buildpack: 'go-brat-buildpack',
-          stack: stack
-        )
-      end
-      let(:browser) { Machete::Browser.new(app) }
-
-      after { Machete::CF::DeleteApp.new.execute(app) }
-
-      it 'runs a simple webserver', version: options[:version] do
-        assert_correct_version_installed(version)
-        assert_root_contains('Hello, World')
+  ['cflinuxfs2'].each do |stack|
+    context "on the #{stack} stack", stack: stack do
+      go_versions = dependency_versions_in_manifest('go','go',stack)
+      go_versions.each do |go_version|
+        it_behaves_like :a_deploy_of_go_app_to_cf, go_version, stack
       end
     end
   end
-
-  context 'On cflinuxfs2 stack', stack: 'cflinuxfs2' do
-    let(:stack) { 'cflinuxfs2' }
-
-    dependencies.each do |dependency|
-      if dependency['name'] == 'go'
-        create_test_for("#{dependency['name']} #{dependency['version']}", version: dependency['version'])
-      end
-    end
-  end
-
-
-  def assert_correct_version_installed(version)
-    expect(app).to be_running(120)
-    expect(app).to have_logged "Installing go#{version}"
-  end
-
-  def assert_root_contains(text)
-    2.times do
-      browser.visit_path('/')
-      expect(browser).to have_body(text)
-    end
-  end
-
 end
