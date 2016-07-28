60f7500697a248d1abbb299cb28923f76ea2044c
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-07-28 14:16:18 -0400

    Add filter credentials test to jruby spec
    - refactor to bring in line with style of other BRATs specs
    
    [#126514693]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- cf_spec/integration/jruby_spec.rb
+++ cf_spec/integration/jruby_spec.rb
@@ -1,19 +1,25 @@
 require 'spec_helper'
 require 'bcrypt'
 
+def deploy_jruby_app(ruby_version,jruby_version,stack)
+  template = JRubyTemplateApp.new(ruby_version,jruby_version)
+  template.generate!
+
+  Machete.deploy_app(
+    template.path,
+    name: template.name,
+    buildpack: 'ruby-brat-buildpack',
+    stack: stack
+  )
+end
+
+
 RSpec.shared_examples :a_deploy_of_jruby_app_to_cf do |ruby_version, jruby_version, stack|
   context "with JRuby version #{jruby_version} and Ruby version #{ruby_version}", version: ruby_version do
     let(:browser) { Machete::Browser.new(@app) }
 
     before(:all) do
-      generate_app('simple_brats', ruby_version, 'jruby', jruby_version)
-
-      @app = Machete.deploy_app(
-        "jruby/tmp/#{ruby_version}/simple_brats",
-        name: "simple-jruby-#{Time.now.to_i}",
-        buildpack: 'ruby-brat-buildpack',
-        stack: stack
-      )
+      @app = deploy_jruby_app(ruby_version, jruby_version, stack)
     end
 
     after(:all) { Machete::CF::DeleteApp.new.execute(@app) }
@@ -75,49 +81,54 @@ RSpec.shared_examples :a_deploy_of_jruby_app_to_cf do |ruby_version, jruby_versi
       end
     end
   end
+end
 
-  def generate_app(app_name, ruby_version, engine, engine_version)
-    origin_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'jruby', app_name)
-    copied_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'jruby', 'tmp', ruby_version, app_name)
-    FileUtils.rm_rf(copied_template_path)
-    FileUtils.mkdir_p(File.dirname(copied_template_path))
-    FileUtils.cp_r(origin_template_path, copied_template_path)
+describe 'For JRuby in the ruby buildpack', language: 'ruby' do
+  describe 'For all supported JRuby versions' do
+    before(:all) do
+      cleanup_buildpack(buildpack: 'ruby')
+      install_buildpack(buildpack: 'ruby')
+    end
 
-    ['Gemfile', '.jrubyrc'].each do |file|
-      evaluate_erb(File.join(copied_template_path, file), binding)
+    ['cflinuxfs2'].each do |stack|
+      context "On #{stack} stack", stack: stack do
+
+      jruby_versions = dependency_versions_in_manifest('ruby','jruby',stack)
+
+      jruby_versions.each do |jruby_version_string|
+          match_data = jruby_version_string.match(/ruby-(.*)-jruby-(.*)/)
+          ruby_version = match_data[1]
+          jruby_version = match_data[2]
+          it_behaves_like :a_deploy_of_jruby_app_to_cf, ruby_version, jruby_version, stack
+        end
+      end
     end
   end
 
-  def evaluate_erb(file_path, our_binding)
-    template = File.read(file_path)
-    f = File.open(file_path, 'w')
-    f << ERB.new(template).result(our_binding)
-    f.close
-  end
-end
+  describe 'staging with custom buildpack that uses credentials in manifest dependency uris' do
+    let(:stack)                { 'cflinuxfs2' }
+    let(:jruby_version_string) { dependency_versions_in_manifest('ruby', 'jruby', stack).last }
+    let(:app) do
+      jruby_version_string.match(/ruby-(.*)-jruby-(.*)/)
+      ruby_version = $1
+      jruby_version = $2
+      deploy_jruby_app(ruby_version,jruby_version,stack)
+    end
 
-describe 'For all supported JRuby versions', language: 'ruby' do
-  before(:all) do
-    cleanup_buildpack(buildpack: 'ruby')
-    install_buildpack(buildpack: 'ruby')
-  end
+    before do
+      cleanup_buildpack(buildpack: 'ruby')
+      install_buildpack_with_uri_credentials(buildpack: 'ruby')
+    end
 
-  def self.dependencies
-    parsed_manifest(buildpack: 'ruby')
-      .fetch('dependencies')
-  end
+    after { Machete::CF::DeleteApp.new.execute(app) }
 
-  ['cflinuxfs2'].each do |stack|
-    context "On #{stack} stack", stack: stack do
-      dependencies.select do |dependency|
-        dependency['cf_stacks'].include?(stack)
-      end.each do |dependency|
-        next unless dependency['name'] == 'jruby'
-        match_data = dependency['version'].match(/ruby-(.*)-jruby-(.*)/)
-        ruby_version = match_data[1]
-        jruby_version = match_data[2]
-        it_behaves_like :a_deploy_of_jruby_app_to_cf, ruby_version, jruby_version, stack
-      end
+    it 'does not include credentials in logged dependency uris' do
+      credential_uri = Regexp.new(Regexp.quote('https://') + 'login:password[@]')
+      jruby_uri = Regexp.new(Regexp.quote('https://-redacted-:-redacted-@buildpacks.cloudfoundry.org/concourse-binaries/jruby/jruby-') + '[\d\.]+' +
+                             Regexp.quote('_ruby-') + '[\d\.]+' + Regexp.quote('-linux-x64.tgz'))
+
+      expect(app).to_not have_logged(credential_uri)
+      expect(app).to have_logged(jruby_uri)
     end
   end
 end
---
+++ cf_spec/support/jruby_template_app.rb
@@ -0,0 +1,40 @@
+class JRubyTemplateApp
+  attr_reader :ruby_version
+  attr_reader :engine_version
+
+  def initialize(ruby_version, engine_version)
+    @ruby_version = ruby_version
+    @engine_version = engine_version
+  end
+
+  def path
+    Shellwords.shellescape("jruby/tmp/#{ruby_version}/simple_brats")
+  end
+
+  def name
+    @name ||= "simple-jruby-#{Time.now.to_i}"
+  end
+
+  def generate!
+    generate_app('simple_brats', ruby_version, 'jruby', engine_version)
+  end
+
+  def generate_app(app_name, ruby_version, engine, engine_version)
+    origin_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'jruby', app_name)
+    copied_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'jruby', 'tmp', ruby_version, app_name)
+    FileUtils.rm_rf(copied_template_path)
+    FileUtils.mkdir_p(File.dirname(copied_template_path))
+    FileUtils.cp_r(origin_template_path, copied_template_path)
+
+    ['Gemfile','.jrubyrc'].each do |file|
+      evaluate_erb(File.join(copied_template_path, file), binding)
+    end
+  end
+
+  def evaluate_erb(file_path, our_binding)
+    template = File.read(file_path)
+    f = File.open(file_path, 'w')
+    f << ERB.new(template).result(our_binding)
+    f.close
+  end
+end
