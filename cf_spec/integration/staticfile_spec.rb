f375fe1cacc16bef2d2cfc0bfe2c1cb34e4fcd3d
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-07-28 17:09:21 -0400

    Add staticfile spec which checks for credentials
    [#126514693]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ cf_spec/integration/staticfile_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'bcrypt'
+
+def deploy_staticfile_app(stack)
+  template = StaticfileTemplateApp.new()
+  template.generate!
+
+  Machete.deploy_app(
+    template.path,
+    name: template.name,
+    buildpack: 'staticfile-brat-buildpack',
+    stack: stack
+  )
+end
+
+describe 'For the staticfile buildpack', language: 'staticfile' do
+
+  describe 'staging with custom buildpack that uses credentials in manifest dependency uris' do
+    let(:stack)        { 'cflinuxfs2' }
+    let(:nginx_version) { dependency_versions_in_manifest('staticfile', 'nginx', stack).last }
+    let(:app)          { deploy_staticfile_app(stack) }
+
+    before do
+      cleanup_buildpack(buildpack: 'staticfile')
+      install_buildpack_with_uri_credentials(buildpack: 'staticfile')
+    end
+
+    after { Machete::CF::DeleteApp.new.execute(app) }
+
+    it 'does not include credentials in logged dependency uris' do
+      credential_uri = Regexp.new(Regexp.quote('https://') + 'login:password[@]')
+      staticfile_uri = Regexp.new(Regexp.quote('https://-redacted-:-redacted-@buildpacks.cloudfoundry.org/concourse-binaries/nginx/nginx-') + '[\d\.]+' + Regexp.quote('-linux-x64.tgz'))
+
+      expect(app).to_not have_logged(credential_uri)
+      expect(app).to have_logged(staticfile_uri)
+    end
+  end
+end
