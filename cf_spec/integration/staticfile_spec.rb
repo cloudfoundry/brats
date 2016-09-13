8aeb25fbe45930bea44bd0f418ad2d28586f7b05
James Wen <jrw2175@columbia.edu>
2016-09-13 18:19:00 -0400

    Add .profile functionality integration specs for staticfile
    [#128334397]

--- cf_spec/integration/staticfile_spec.rb
+++ cf_spec/integration/staticfile_spec.rb
@@ -1,23 +1,20 @@
 require 'spec_helper'
 require 'bcrypt'
 
-def deploy_staticfile_app(stack)
+def generate_staticfile_app
   template = StaticfileTemplateApp.new()
   template.generate!
-
-  Machete.deploy_app(
-    template.path,
-    name: template.name,
-    buildpack: 'staticfile-brat-buildpack',
-    stack: stack
-  )
+  template
 end
 
 describe 'For the staticfile buildpack', language: 'staticfile' do
   describe 'staging with custom buildpack that uses credentials in manifest dependency uris' do
     let(:stack)         { 'cflinuxfs2' }
     let(:nginx_version) { dependency_versions_in_manifest('staticfile', 'nginx', stack).last }
-    let(:app)           { deploy_staticfile_app(stack) }
+    let(:app) do
+      app_template = generate_staticfile_app
+      deploy_app(template: app_template, stack: stack, buildpack: 'staticfile-brat-buildpack')
+    end
 
     before do
       cleanup_buildpack(buildpack: 'staticfile')
@@ -46,6 +43,27 @@ describe 'For the staticfile buildpack', language: 'staticfile' do
         expect(app).to_not have_logged(credential_uri)
         expect(app).to have_logged(staticfile_uri)
       end
+    end
+  end
+
+  describe 'deploying an app that has an executable .profile script' do
+    let(:stack)          { 'cflinuxfs2' }
+    let(:app) do
+      app_template = generate_staticfile_app
+      add_dot_profile_script_to_app(app_template.full_path)
+      deploy_app(template: app_template, stack: stack, buildpack: 'staticfile-brat-buildpack')
+    end
+
+    before(:all) do
+      skip_if_no_dot_profile_support_on_targeted_cf
+      cleanup_buildpack(buildpack: 'staticfile')
+      install_buildpack(buildpack: 'staticfile')
+    end
+
+    after { Machete::CF::DeleteApp.new.execute(app) }
+
+    it 'executes the .profile script' do
+      expect(app).to have_logged("PROFILE_SCRIPT_IS_PRESENT_AND_RAN")
     end
   end
 end
--- cf_spec/support/staticfile_template_app.rb
+++ cf_spec/support/staticfile_template_app.rb
@@ -1,4 +1,6 @@
 class StaticfileTemplateApp
+  attr_reader :full_path
+
   def path
     "staticfile/tmp/simple_brats"
   end
@@ -10,6 +12,7 @@ class StaticfileTemplateApp
   def generate!
     origin_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'staticfile', 'simple_brats')
     copied_template_path = File.join(File.dirname(__FILE__), '..', 'fixtures', 'staticfile', 'tmp', 'simple_brats')
+    @full_path = File.expand_path(copied_template_path)
     FileUtils.rm_rf(copied_template_path)
     FileUtils.mkdir_p(File.dirname(copied_template_path))
     FileUtils.cp_r(origin_template_path, copied_template_path)
