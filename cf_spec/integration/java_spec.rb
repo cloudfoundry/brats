9be4313b607d748fdf9d36e760bffe4e7c97e87f
Sam Smith <sesmith177@gmail.com>
2017-03-16 12:26:49 -0400

    Update java fixture to account for JBP memory changes
    [#141866095]
    
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- cf_spec/integration/java_spec.rb
+++ cf_spec/integration/java_spec.rb
@@ -1,28 +1,38 @@
 require 'spec_helper'
 
 describe 'For the java buildpack', language: 'java' do
+  # java buildpack doesn't have a VERSION file
+  before(:all) do
+    @old_buildpack_version = ENV['BUILDPACK_VERSION']
+    ENV['BUILDPACK_VERSION'] = 'not_relevant'
+
+    cleanup_buildpack(buildpack: 'java')
+    install_java_buildpack(position: 1)
+  end
+
   after(:all) do
     cleanup_buildpack(buildpack: 'java')
+    ENV['BUILDPACK_VERSION'] = @old_buildpack_version
   end
 
   describe 'deploying an app that has sensitive environment variables' do
-    let(:app) do
+    before do
       template = JavaTemplateApp.new
-      Machete.deploy_app(
+      @app = Machete.deploy_app(
         template.path,
         name: template.name,
-        service: true
+        manifest: template.manifest,
+        service: true,
+        buildpack: 'java-brat-buildpack',
+        skip_verify_version: true
       )
     end
 
-    before(:all) do
-      cleanup_buildpack(buildpack: 'java')
-      install_java_buildpack(position: 1)
-    end
+    after(:all) { Machete::CF::DeleteApp.new.execute(@app) }
 
     it 'will not write credentials to the app droplet' do
-      expect(app).to be_running
-      expect(app.name).to keep_credentials_out_of_droplet
+      expect(@app).to be_running
+      expect(@app.name).to keep_credentials_out_of_droplet
     end
   end
 end
--- cf_spec/support/java_template_app.rb
+++ cf_spec/support/java_template_app.rb
@@ -3,6 +3,10 @@ class JavaTemplateApp
     'java/sample.war'
   end
 
+  def manifest
+    File.join(File.dirname(__FILE__), '..', '..', 'cf_spec/fixtures/java/manifest.yml')
+  end
+
   def name
     @name ||= "simple-java-#{Time.now.to_i}"
   end
