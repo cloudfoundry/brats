ce828f9eea7d8631fbc312d7ff3dbf139585ead9
Dave Goddard <dave@goddard.id.au>
2017-03-23 12:17:53 -0400

    Allow 404 for "/.profile" on staticfile

--- cf_spec/integration/staticfile_spec.rb
+++ cf_spec/integration/staticfile_spec.rb
@@ -94,7 +94,7 @@ describe 'For the staticfile buildpack', language: 'staticfile' do
     end
 
     it 'does not let me view the .profile script' do
-      browser.visit_path('/.profile')
+      browser.visit_path('/.profile', allow_404: true)
       expect(browser).to_not have_body 'PROFILE_SCRIPT_IS_PRESENT_AND_RAN'
     end
   end
